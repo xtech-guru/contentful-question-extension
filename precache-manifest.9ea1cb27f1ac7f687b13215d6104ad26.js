734a3c8a525f809689444a375924b488b663eb20
Zied Haj Salah <ziedhs@xtech.guru>
2020-09-18 08:35:31 +0100

    Updates

---
+++ precache-manifest.9ea1cb27f1ac7f687b13215d6104ad26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bc3a8c54616b9db23eea12170cbf35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38cd544eac37dad362bb",
+    "url": "/static/css/2.a1a9edf9.chunk.css"
+  },
+  {
+    "revision": "932a4ff872ed2f2cfd0a",
+    "url": "/static/css/main.7c0c0cd0.chunk.css"
+  },
+  {
+    "revision": "38cd544eac37dad362bb",
+    "url": "/static/js/2.e8f4c25d.chunk.js"
+  },
+  {
+    "revision": "b674ba54ed1da4a284a9f2dbd781a504",
+    "url": "/static/js/2.e8f4c25d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "932a4ff872ed2f2cfd0a",
+    "url": "/static/js/main.473284ee.chunk.js"
+  },
+  {
+    "revision": "e354d8b8dd59ed580fd3",
+    "url": "/static/js/runtime-main.68cc1e8c.js"
+  }
+]);
